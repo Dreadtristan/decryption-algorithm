f3c1cb952a154ec3baae69998fae1278103d0da4
Dreadtristan <94937729+Dreadtristan@users.noreply.github.com>
2021-11-23 18:57:52 -0500

    Add files via upload
    add the code

---
+++ decoder2.py
@@ -0,0 +1,115 @@
+coder = []
+inpots = 0
+while True:
+    inpots = input("decoder: ")
+    if inpots == "print":
+        print(coder)
+    if inpots == "clear":
+        coder = []
+    if inpots == "b":
+        coder.append("a")
+
+    if inpots == "c":
+        coder.append("b")
+
+    if inpots == "7":
+        coder.append("c")
+
+    if inpots == "q":
+        coder.append("d")
+
+    if inpots == "9":
+        coder.append("e")
+
+    if inpots == "w":
+        coder.append("f")
+
+    if inpots == "d":
+        coder.append("g")
+
+    if inpots == "2":
+        coder.append("h")
+
+    if inpots == "r":
+        coder.append("i")
+
+    if inpots == "m":
+        coder.append("j")
+
+    if inpots == "n":
+        coder.append("k")
+
+    if inpots == "s":
+        coder.append("l")
+
+    if inpots == "l":
+        coder.append("m")
+
+    if inpots == "k":
+        coder.append("n")
+
+    if inpots == "i":
+        coder.append("o")
+
+    if inpots == "0":
+        coder.append("p")
+
+    if inpots == "u":
+        coder.append("q")
+
+    if inpots == "f":
+        coder.append("r")
+
+    if inpots == "1":
+        coder.append("s")
+
+    if inpots == "v":
+        coder.append("t")
+
+    if inpots == "5":
+        coder.append("u")
+
+    if inpots == "3":
+        coder.append("v")
+
+    if inpots == "4":
+        coder.append("w")
+
+    if inpots == "y":
+        coder.append("x")
+
+    if inpots == "z":
+        coder.append("y")
+
+    if inpots == "a":
+        coder.append("z")
+
+    if inpots == "8":
+        coder.append("1")
+
+    if inpots == "6":
+        coder.append("2")
+
+    if inpots == "p":
+        coder.append("3")
+
+    if inpots == "h":
+        coder.append("4")
+
+    if inpots == "t":
+        coder.append("5")
+
+    if inpots == "g":
+        coder.append("6")
+
+    if inpots == "x":
+        coder.append("7")
+
+    if inpots == "e":
+        coder.append("8")
+
+    if inpots == "o":
+        coder.append("9")
+
+    if inpots == "j":
+        coder.append("0")
